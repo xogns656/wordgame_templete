05311bdeb0bf58a13402a10a57b6144d6ed99af2
mininerej <ejhong.4991@gmail.com>
2019-07-19 15:39:56 +0900

    feat: initial commit

---
+++ minigame/minigame/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for minigame project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minigame.settings')
+
+application = get_wsgi_application()
---
+++ minigame/word_chain/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WordChainConfig(AppConfig):
+    name = 'word_chain'
