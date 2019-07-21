6e4643301bff8b9f1daa16651dd9b9f1bb8071ed
mininerej <ejhong.4991@gmail.com>
2019-07-21 15:58:58 +0900

    feat: initial commit

---
+++ minigame/word_chain/utils.py
@@ -0,0 +1,18 @@
+from word_chain.consts import INITIAL_SOUND_SET
+
+
+def check_match(agent_word: str, user_query: str):
+    """
+    :param agent_word: a word sent by the agent
+    :type agent_word: basestring
+    :param user_query: a word sent by the user
+    :type user_query: basestring
+    :return: whether the first syllable of a word sent by the user is
+            the same as the last syllable of the word sent by the agent.
+            This takes into account du-eum law - to avoid pronouncing a
+            sound at the beginning of a word, read in a different pronunciation.
+    """
+    if agent_word[-1] in INITIAL_SOUND_SET:
+        return INITIAL_SOUND_SET[agent_word[-1]] == user_query[0]
+    else:
+        return agent_word[-1] == user_query[0]
